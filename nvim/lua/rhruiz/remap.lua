71957f9986ccaaaf8e0b6151481dd1d784fde575
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2023-05-20 16:54:04 -0300

    handle absolute paths

--- nvim/lua/rhruiz/remap.lua
+++ nvim/lua/rhruiz/remap.lua
@@ -4,9 +4,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set("n", "<leader>fy", function()
-    vim.fn.setreg("", vim.fn.expand("%"))
+    vim.fn.setreg("", vim.fn.fnamemodify(vim.fn.expand('%:p'), ':p:~:.'))
 end)
 
 vim.keymap.set("n", "<leader>fY", function()
-    vim.fn.setreg("+", vim.fn.expand("%"))
+    vim.fn.setreg("+", vim.fn.fnamemodify(vim.fn.expand('%:p'), ':p:~:.'))
 end)
