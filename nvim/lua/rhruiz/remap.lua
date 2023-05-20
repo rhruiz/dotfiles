04d5419bb256599f8b67cffc72e5fc59aa4aa76a
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2023-05-20 16:48:50 -0300

    yank file name to registers

--- nvim/lua/rhruiz/remap.lua
+++ nvim/lua/rhruiz/remap.lua
@@ -2,3 +2,11 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>fy", function()
+    vim.fn.setreg("", vim.fn.expand("%"))
+end)
+
+vim.keymap.set("n", "<leader>fY", function()
+    vim.fn.setreg("+", vim.fn.expand("%"))
+end)
