0bd21c330ee7e43501ad16c915c8a3fe9ef5e488
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2023-10-15 01:06:39 -0300

    fix autocmd

--- nvim/lua/rhruiz/packer.lua
+++ nvim/lua/rhruiz/packer.lua
@@ -5,7 +5,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use { 'HiPhish/nvim-ts-rainbow2' }
-  use { 'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = { {'nvim-lua/plenary.nvim'} } }
+  use { 'nvim-telescope/telescope.nvim', tag = '0.1.4', requires = { {'nvim-lua/plenary.nvim'} } }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use {
       'nvim-lualine/lualine.nvim',
--- nvim/lua/rhruiz/ruby.lua
+++ nvim/lua/rhruiz/ruby.lua
@@ -1,3 +1,3 @@
 vim.g.ruby_host_prog = '/usr/local/lib/ruby/gems/3.2.0/bin/neovim-ruby-host'
 
-vim.api.nvim_create_autocmd('BufRead,BufNewFile', { pattern = 'Gemfile', command = 'setf ruby' })
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, { pattern = 'Gemfile', command = 'setf ruby' })
