942a7a31ec3a8e09de909601e63686bf189839e0
Ricardo Ruiz <ricardo.ruiz@locaweb.com.br>
2024-05-30 17:20:41 -0300

    Move to new rainbow delimiters

--- nvim/lua/rhruiz/packer.lua
+++ nvim/lua/rhruiz/packer.lua
@@ -4,7 +4,7 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use { 'HiPhish/nvim-ts-rainbow2' }
+  use { 'HiPhish/rainbow-delimiters.nvim' }
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.4', requires = { {'nvim-lua/plenary.nvim'} } }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use {
