437ab64b10aeeb51e475dc059d210b85176bbb76
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2024-06-22 21:16:52 -0300

    every true color terminal gets darkness

--- nvim/lua/rhruiz/init.lua
+++ nvim/lua/rhruiz/init.lua
@@ -1,11 +1,14 @@
 inUtterDarkness = function()
-	return (vim.fn.has("termguicolors") or vim.env.GNOME_TERMINAL_SCREEN ~= nil)
+	if vim.env.COLORTERM == "truecolor" then
+		return true
+	end
+
+	return vim.env.GNOME_TERMINAL_SCREEN ~= nil
 		and vim.env.TERM_PROGRAM ~= "Apple_Terminal"
 		and vim.env.ITERM_PROFILE ~= "Spacemacs light"
 end
 
 require("rhruiz/settings")
--- require("rhruiz/packer")
 require("rhruiz/lazy")
 pcall(function()
 	require("rhruiz/local_settings")
