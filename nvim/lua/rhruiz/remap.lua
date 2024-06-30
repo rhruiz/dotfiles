27c11565b5b7830d3a4c0e89b3fe408fc4f4b5c1
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2024-06-30 13:10:01 -0300

    +which-key

--- nvim/lua/rhruiz/remap.lua
+++ nvim/lua/rhruiz/remap.lua
@@ -3,10 +3,12 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
-vim.keymap.set("n", "<leader>fy", function()
-    vim.fn.setreg("", vim.fn.fnamemodify(vim.fn.expand('%:p'), ':p:~:.'))
-end)
+local wk = require('which-key')
 
-vim.keymap.set("n", "<leader>fY", function()
-    vim.fn.setreg("+", vim.fn.fnamemodify(vim.fn.expand('%:p'), ':p:~:.'))
-end)
+wk.register({
+	["<leader>f"] = {
+		name = "+file",
+		y = { function() vim.fn.setreg("", vim.fn.fnamemodify(vim.fn.expand('%:p'), ':p:~:.')) end, 'Yank file path' },
+		Y = { function() vim.fn.setreg("+", vim.fn.fnamemodify(vim.fn.expand('%:p'), ':p:~:.')) end, 'Yank file path to clipboard' },
+	},
+})
