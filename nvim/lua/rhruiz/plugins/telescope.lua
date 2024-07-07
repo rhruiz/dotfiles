eb55b2228f356d4285e20216027a317b223c17ef
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2024-07-07 00:43:07 -0300

    modularize lazy.lua

---
+++ nvim/lua/rhruiz/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+       'nvim-tree/nvim-web-devicons',
+    }
+  }
---
+++ nvim/lua/rhruiz/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'elixir', 'ruby' },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.install').prefer_git = true
+      require('nvim-treesitter.configs').setup(opts)
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require('treesitter-context').setup({
+        max_lines = 6,
+      })
+    end,
+  }
+}
