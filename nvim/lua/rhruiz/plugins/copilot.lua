87288358a2a3cc8fbdef9acce922adef252c8c3c
Ricardo Ruiz <ricardo.ruiz@locaweb.com.br>
2024-07-10 11:43:17 -0300

    Copilot and nvim-cmp sitting in a tree...

--- nvim/lua/rhruiz/plugins/copilot.lua
+++ nvim/lua/rhruiz/plugins/copilot.lua
@@ -1,13 +1,24 @@
 return {
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function ()
+      require("copilot_cmp").setup()
+    end
+  },
+  {
   -- 'github/copilot.vim',
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
     require("copilot").setup({
+      panel = {
+        enabled = false,
+      },
       suggestion = {
-        auto_trigger = true
+        enabled = false,
+        -- auto_trigger = true
       }
     })
   end,
-}
+}}
