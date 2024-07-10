e9c3f02ea1f7f8e62c2c473a10266fa1cdf8557e
Ricardo Ruiz <ricardo.ruiz@locaweb.com.br>
2024-07-10 11:43:17 -0300

    Copilot and nvim-cmp sitting in a tree...

--- nvim/lua/rhruiz/plugins/completion.lua
+++ nvim/lua/rhruiz/plugins/completion.lua
@@ -105,6 +105,7 @@ return {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'copilot', group_index = 2 },
         },
       }
     end,
