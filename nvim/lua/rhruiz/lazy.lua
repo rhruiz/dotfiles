d4726884e93e180029b3435419bfa14e35a8e192
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2024-07-13 19:36:15 -0300

    chore(nvim): prioritize every colorscheme

--- nvim/lua/rhruiz/lazy.lua
+++ nvim/lua/rhruiz/lazy.lua
@@ -61,12 +61,12 @@ require("lazy").setup({
   },
 
   -- colorschemes,
-  'liuchengxu/space-vim-theme',
-  { 'joshdick/onedark.vim' },
+  { 'liuchengxu/space-vim-theme', priority = 1000 },
+  { 'joshdick/onedark.vim', priority = 1000 },
   { 'folke/tokyonight.nvim', priority = 1000 },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { 'romgrk/doom-one.vim' },
-  { 'sonph/onehalf', config = function(p) vim.opt.rtp:append(p.dir .. '/vim')  end},
-  { 'rakr/vim-one' },
+  { 'romgrk/doom-one.vim', priority = 1000 },
+  { 'sonph/onehalf', priority = 1000, config = function(p) vim.opt.rtp:append(p.dir .. '/vim')  end},
+  { 'rakr/vim-one', priority = 1000 },
   { import = 'rhruiz.plugins' },
 })
