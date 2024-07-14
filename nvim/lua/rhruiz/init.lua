9ff2dc0f91fd1b32e34bef17969bbe7570d44006
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2024-07-14 10:14:38 -0300

    feat(nvim): use terminal background color in Terminal.app

--- nvim/lua/rhruiz/init.lua
+++ nvim/lua/rhruiz/init.lua
@@ -1,18 +1,17 @@
-
-local colors = require("rhruiz/colors")
 require("rhruiz/settings")
 pcall(function() require("rhruiz/local_settings") end)
 require("rhruiz/lazy")
 require("rhruiz/remap")
 require("rhruiz/ruby")
 
-vim.opt.termguicolors = vim.env.TERM_PROGRAM ~= "Apple_Terminal"
-
-if colors.inUtterDarkness() then
+if require("rhruiz/colors").inUtterDarkness() then
     vim.g.doom_one_terminal_colors = true
     vim.o.background = 'dark'
     vim.cmd [[colorscheme onehalfdark]]
 else
     vim.o.background = 'light'
     vim.cmd [[colorscheme space_vim_theme]]
+    vim.cmd [[hi Normal ctermbg=NONE guibg=NONE]]
+    vim.cmd [[hi LineNr ctermbg=NONE guibg=NONE]]
+    vim.cmd [[hi SignColumn ctermbg=NONE guibg=NONE]]
 end
