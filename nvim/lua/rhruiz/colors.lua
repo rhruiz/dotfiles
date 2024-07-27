cc927b7f72e96b895c6c8c907e490a31668b86c5
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2024-07-27 20:11:52 -0300

    feat(nvim): move transparent_bg do rhruiz.colors to make it callable from command mode

--- nvim/lua/rhruiz/colors.lua
+++ nvim/lua/rhruiz/colors.lua
@@ -2,16 +2,22 @@ local colors = {}
 
 local runtime = require("rhruiz.runtime")
 
-function colors.is_dark_mode()
-		if runtime.is_kitty() then
-				return false
-		end
-		if runtime.is_truecolor() then
-				return true
-		end
+function colors.transparent_bg()
+	vim.cmd [[hi Normal ctermbg=NONE guibg=NONE]]
+	vim.cmd [[hi LineNr ctermbg=NONE guibg=NONE]]
+	vim.cmd [[hi SignColumn ctermbg=NONE guibg=NONE]]
+end
 
-    return not runtime.is_terminal_app()
-				and vim.env.ITERM_PROFILE ~= "Spacemacs light"
+function colors.is_dark_mode()
+	if runtime.is_kitty() then
+		return false
+	end
+	if runtime.is_truecolor() then
+		return true
+	end
+
+	return not runtime.is_terminal_app()
+		and vim.env.ITERM_PROFILE ~= "Spacemacs light"
 end
 
 return colors
--- nvim/lua/rhruiz/init.lua
+++ nvim/lua/rhruiz/init.lua
@@ -4,23 +4,18 @@ require("rhruiz.lazy")
 require("rhruiz.remap")
 require("rhruiz.ruby")
 local runtime = require("rhruiz.runtime")
-
-local transparent_bg = function()
-		vim.cmd [[hi Normal ctermbg=NONE guibg=NONE]]
-		vim.cmd [[hi LineNr ctermbg=NONE guibg=NONE]]
-		vim.cmd [[hi SignColumn ctermbg=NONE guibg=NONE]]
-end
+local colors = require("rhruiz.colors")
 
 if require("rhruiz.colors").is_dark_mode() then
-		vim.g.doom_one_terminal_colors = true
-		vim.o.background = 'dark'
-		vim.cmd [[colorscheme onehalfdark]]
+	vim.g.doom_one_terminal_colors = true
+	vim.o.background = 'dark'
+	vim.cmd [[colorscheme onehalfdark]]
 
-		if runtime.is_wezterm() and runtime.is_mac() then
-				transparent_bg()
-		end
+	if runtime.is_wezterm() and runtime.is_mac() then
+		colors.transparent_bg()
+	end
 else
-		vim.o.background = 'light'
-		vim.cmd [[colorscheme space_vim_theme]]
-		transparent_bg()
+	vim.o.background = 'light'
+	vim.cmd [[colorscheme space_vim_theme]]
+	colors.transparent_bg()
 end
