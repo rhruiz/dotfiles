a8279e24c9dafd7753a025121b74eccf6dfe70a6
Ricardo Ruiz <ricardo.ruiz@locaweb.com.br>
2024-08-05 09:49:03 -0300

    feat(wezterm): Tweak catppuccin tabs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -47,41 +47,39 @@ local tab_title = function(tab_info)
   return tab_info.active_pane.title
 end
 
-wezterm.on(
-  'format-tab-title',
-  function(tab, _, _, _, _, max_width)
-    local tab_bar_background = config.colors.tab_bar.background
-    local edge_background = config.colors.tab_bar.active_tab.bg_color
-    local edge_foreground = config.colors.tab_bar.inactive_tab.bg_color
-    local background = config.colors.tab_bar.inactive_tab_edge
-    local foreground = config.colors.foreground
+wezterm.on('format-tab-title', function(tab, _, _, _, _, max_width)
+  local tab_bar_background = config.colors.tab_bar.background
+  local edge_background = config.colors.tab_bar.active_tab.bg_color
+  local edge_foreground = config.colors.tab_bar.inactive_tab.bg_color
+  local background = config.colors.tab_bar.inactive_tab_edge
+  local foreground = config.colors.foreground
 
-    if tab.is_active then
-      edge_background = config.colors.compose_cursor
-      edge_foreground = config.colors.tab_bar.active_tab.fg_color
-    end
-
-    local title = tab_title(tab)
-    title = wezterm.truncate_right(title, max_width - 2)
-
-    return {
-      { Background = { Color = tab_bar_background } },
-      { Foreground = { Color = background } },
-      -- { Text = wezterm.nerdfonts.ple_lower_right_triangle },
-      { Text = wezterm.nerdfonts.ple_left_half_circle_thick },
-      { Background = { Color = background } },
-      { Foreground = { Color = foreground } },
-      { Text = title .. " " },
-      { Background = { Color = edge_background } },
-      { Foreground = { Color = edge_foreground } },
-      { Text = " " .. tostring(tab.tab_index) },
-      { Background = { Color = tab_bar_background } },
-      { Foreground = { Color = edge_background } },
-      { Text = wezterm.nerdfonts.ple_right_half_circle_thick },
-      -- { Text = wezterm.nerdfonts.ple_upper_left_triangle },
-      { Text = " " },
-    }
+  if tab.is_active then
+    edge_background = config.colors.compose_cursor
+    edge_foreground = config.colors.tab_bar.active_tab.fg_color
   end
-)
+
+  local title = tab_title(tab)
+  title = wezterm.truncate_right(title, max_width - 6)
+
+  return {
+    { Background = { Color = tab_bar_background } },
+    { Foreground = { Color = background } },
+    { Text = not is_mac and tab.tab_index == 0 and " " or "" },
+    -- { Text = wezterm.nerdfonts.ple_lower_right_triangle },
+    { Text = wezterm.nerdfonts.ple_left_half_circle_thick },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title .. " " },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = " " .. tostring(tab.tab_index) },
+    { Background = { Color = tab_bar_background } },
+    { Foreground = { Color = edge_background } },
+    { Text = wezterm.nerdfonts.ple_right_half_circle_thick },
+    -- { Text = wezterm.nerdfonts.ple_upper_left_triangle },
+    { Text = " " },
+  }
+end)
 
 return config
