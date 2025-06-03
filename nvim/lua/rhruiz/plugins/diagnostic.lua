a3f04938b8faecc85b89bf860913394d4265818f
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2025-06-03 18:38:47 -0300

    fix(nvim): trouble diagnostics only for current buffer

--- nvim/lua/rhruiz/plugins/diagnostic.lua
+++ nvim/lua/rhruiz/plugins/diagnostic.lua
@@ -4,7 +4,7 @@ return {
     cmd = "Trouble",
     keys = {
       { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
-      { "<leader>xd", "<cmd>Trouble symbols<cr>", desc = "Document Diagnostics (Trouble)" },
+      { "<leader>xd", "<cmd>Trouble diagnostics toggle focus=false filter.buf=0<cr>", desc = "Document Diagnostics (Trouble)" },
       { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
       { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
     },
