155f2cfe33b0ac8b7430ce07f51145087da71e91
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2025-06-03 18:35:42 -0300

    fix(nvim): update diagnostic/trouble settings

--- nvim/lua/rhruiz/plugins/diagnostic.lua
+++ nvim/lua/rhruiz/plugins/diagnostic.lua
@@ -3,11 +3,10 @@ return {
     "folke/trouble.nvim",
     cmd = "Trouble",
     keys = {
-      { "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Diagnostics (Trouble)" },
-      { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-      { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-      { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
-      { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xd", "<cmd>Trouble symbols<cr>", desc = "Document Diagnostics (Trouble)" },
+      { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
     },
     opts = {
       auto_open = false,
