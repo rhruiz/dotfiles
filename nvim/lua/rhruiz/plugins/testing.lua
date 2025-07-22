638546d334940e3f9c9686d0200475fba78a73d5
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2025-07-22 10:22:41 -0300

    feat(nvim): add neotest bindings

--- nvim/lua/rhruiz/plugins/testing.lua
+++ nvim/lua/rhruiz/plugins/testing.lua
@@ -6,13 +6,21 @@ return {
     "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
     "jfpedroza/neotest-elixir",
+    "nvim-neotest/neotest-plenary",
   },
   config = function ()
     require("neotest").setup({
       adapters = {
         require("neotest-elixir"),
+        require("neotest-plenary"),
       }
+    })
+
+    local run = require("neotest").run.run
+
+    require('which-key').add({
+      { '<leader>tr', run, desc = '[T]est: [R]un' },
+      { '<leader>tf', function() run(vim.fn.expand("%")) end, desc = '[T]est: Run [F]ile' },
     })
   end
 }
-
