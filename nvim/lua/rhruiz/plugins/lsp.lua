1bf03c8db28f16b4979a507dea6293d45ad74aff
Ricardo Hermida Ruiz <ricardo.hermida@nubank.com.br>
2025-12-23 14:56:46 -0300

    feat(lsp): add code format binding

--- nvim/lua/rhruiz/plugins/lsp.lua
+++ nvim/lua/rhruiz/plugins/lsp.lua
@@ -58,6 +58,8 @@ return {
             vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
+          map('<leader>cf', vim.lsp.buf.format, '[F]ormat [C]ode')
+
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
