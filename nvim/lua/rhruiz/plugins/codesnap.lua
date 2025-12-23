c4fe5eefa20172d6a4751fe23df25b0d67ec92a7
Ricardo Hermida Ruiz <ricardo.hermida@nubank.com.br>
2025-12-23 14:56:27 -0300

    fix(codesnap): pin to older version to keep config

--- nvim/lua/rhruiz/plugins/codesnap.lua
+++ nvim/lua/rhruiz/plugins/codesnap.lua
@@ -1,12 +1,17 @@
 return {
   'mistricky/codesnap.nvim',
+  tag = "v1.6.3",
   build = 'make',
   cond = not require('rhruiz.runtime').vscode,
   config = function()
     require('codesnap').setup({
       code_font_family = "Hack Nerd Font",
+      bg_x_margin = 10,
+      bg_y_margin = 10,
+      bg_margin = nil,
       bg_theme = "grape",
-      save_path = "~/Desktop/"
+      save_path = "~/Desktop/",
+      watermark = "",
     })
   end,
 }
