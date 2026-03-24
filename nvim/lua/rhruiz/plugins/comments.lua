78e3a8916a202766b4fe164b494390c61e198398
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2026-03-24 20:38:08 -0300

    refactor(nvim): use `cond` to check if we need copilot or not

--- nvim/lua/rhruiz/plugins/comments.lua
+++ nvim/lua/rhruiz/plugins/comments.lua
@@ -1,4 +1,14 @@
 return {
   { 'numToStr/Comment.nvim', opts = {} },
-  { 'folke/todo-comments.nvim', cond = not require("rhruiz.runtime").vscode, event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    cond = not require("rhruiz.runtime").vscode,
+    event = 'VimEnter',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    },
+    opts = {
+      signs = false
+    }
+  },
 }
--- nvim/lua/rhruiz/plugins/copilot.lua
+++ nvim/lua/rhruiz/plugins/copilot.lua
@@ -1,3 +1,15 @@
 local runtime = require("rhruiz.runtime")
 
-return {}
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    cond = runtime.load_copilot,
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
+  }
+}
